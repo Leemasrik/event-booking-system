e5d8047c38be3b64fc8f81f72ad3acc5357fa0d4
leema <leemasrikarna@gmail.com>
2025-07-07 00:53:35 +0530

    Clean requirements and fix DB config for psycopg3

--- event_system/settings.py
+++ event_system/settings.py
@@ -137,6 +137,10 @@ import dj_database_url
 
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
